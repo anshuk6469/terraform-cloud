2da3d54dd341052265b288a7dcca0ca8434bcde0
Deepanshu Kumar <86040514+anshuk6469@users.noreply.github.com>
2023-05-26 06:18:39 +0530

    Update ec2..tf

--- ec2..tf
+++ ec2..tf
@@ -19,3 +19,8 @@ resource "aws_instance" "example1" {
   ami           = "ami-016eb5d644c333ccb"
   instance_type = "t2.micro"
 }
+
+resource "aws_instance" "example2" {
+  ami           = "ami-016eb5d644c333ccb"
+  instance_type = "t2.micro"
+}
